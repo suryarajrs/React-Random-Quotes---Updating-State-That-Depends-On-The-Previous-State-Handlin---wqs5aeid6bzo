6fba594cbcd0c8c633dc8ac78e44abe0e11193a7
suryarajrs <suryars@outlook.in>
2024-02-15 14:42:28 +0530

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/App.css";
 
 var colors = [
@@ -18,11 +18,38 @@ var colors = [
 
 const App = () => {
 
+  const [data,setData]=useState({});
+
+  const handleOnclick = async()=>{
+
+    const responce = await fetch("https://api.quotable.io/random")
+    const result  = await responce.json();
+    setData(result)
+    console.log(data)
+    
+
+  }
+
     return (
       <div id="main">
         <div id="wrapper">
+
+          <div id="quote-box">
+          <div className="quote-text">
+            <i>{data.content}</i>
+
+          </div>
+          <div className="quote-author" >
+            <h4>{data.author}</h4>
+ 
+          </div>
+
+          </div>
+          
+          <div></div>
           
         </div>
+          <button onClick={handleOnclick} id="new-quote" className="button buttons">Get Quote</button>
       </div>
     );
 };
